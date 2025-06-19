7f44fcd058601b1379fb592f81a6d6aee3e8fbe3
tiagosousa <tiagosousa.tams@hotmail.com>
2025-06-19 15:17:12 +0100

    instal tailwindcss and prismic

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+import "./globals.css";
+import { PrismicPreview } from "@prismicio/next";
+import { repositoryName } from "@/prismicio";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="bg-red-500">{children}</body>
+      <PrismicPreview repositoryName={repositoryName} />
+    </html>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: ["./src/components/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: ["./src/slices/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
