7a7d76c5e8af465854dc00903d07b3fabd3d9d92
tiagosousa <tiagosousa.tams@hotmail.com>
2025-06-21 20:49:45 +0100

    install react icons and typography from tailwindcss

--- src/slices/Showcase/index.tsx
+++ src/slices/Showcase/index.tsx
@@ -2,7 +2,9 @@ import { FC } from "react";
 import { Content } from "@prismicio/client";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import Bounded from "@/components/Bounded";
-import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
+import { PrismicNextImage } from "@prismicio/next";
+import ButtonLink from "@/components/ButtonLink";
+import clsx from "clsx";
 
 /**
  * Props for `Showcase`.
@@ -20,14 +22,38 @@ const Showcase: FC<ShowcaseProps> = ({ slice }) => {
       className="relative"
     >
       <div className="glow absolute -z-10 aspect-square w-full max-w-xl rounded-full bg-blue-400/20 blur-3xl filter" />
-      <PrismicRichText field={slice.primary.heading} />
-      <PrismicNextImage field={slice.primary.image} />
-      <>{slice.primary.icon}</>
-      <PrismicRichText field={slice.primary.subheading} />
-      <PrismicRichText field={slice.primary.body} />
-      <PrismicNextLink field={slice.primary.button_link}>
-        {slice.primary.button_text}
-      </PrismicNextLink>
+      <PrismicRichText
+        field={slice.primary.heading}
+        components={{
+          heading2: ({ children }) => (
+            <h2 className=" -text-5xl text-balance text-center font-medium md:text-7xl">
+              {children}
+            </h2>
+          ),
+        }}
+      />
+      <div className="grid mt-16 items-center rounded-xl border border-blue-50/20 bg-gradient-to-b from-slate-50/15 to-slate-50/5 px-8 py-8 backdrop-blur-sm lg:grid-cols-3 lg:py-12">
+        <div>
+          <>{slice.primary.icon}</>
+          <div className="mt-6 text-2xl font-normal">
+            <PrismicRichText field={slice.primary.subheading} />
+          </div>
+
+          <div className="mt-4 max-w-xl">
+            <PrismicRichText field={slice.primary.body} />
+          </div>
+          <ButtonLink field={slice.primary.button_link} className="mt-6">
+            {slice.primary.button_text || "Learn more"}
+          </ButtonLink>
+        </div>
+        <PrismicNextImage
+          field={slice.primary.image}
+          className={clsx(
+            "opacity-90 shadow-2xl lg:col-span-2 lg:pt-0",
+            "lg:-order-1 lg:translate-x-[-15%]"
+          )}
+        />
+      </div>
     </Bounded>
   );
 };
