078f0990eec1f770e2a673395b4bd09c0aacafea
tiagosousa <tiagosousa.tams@hotmail.com>
2025-06-27 17:56:06 +0100

    navbar updated v2

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -19,39 +19,38 @@ export const NavBar = ({ settings }: NavBarProps) => {
   const pathname = usePathname();
 
   return (
-    <nav className="px-4 py-4 md:px-6 md-:py-6" aria-label="Main">
-      <div className="mx-auto flex max-w-6xl flex-col justify-between py-2 font-medium text-white md:flex-row md:items-center ">
+    <nav className="md-:py-6 px-4 py-4 md:px-6" aria-label="Main">
+      <div className="mx-auto flex max-w-6xl flex-col justify-between py-2 font-medium text-white md:flex-row md:items-center">
         <div className="flex items-center justify-between">
-          <Link href={"/"}>
+          <Link href="/" className="z-50" onClick={() => setOpen(false)}>
             <WordMark />
             <span className="sr-only">Glisten.ai Home Page</span>
           </Link>
           <button
             type="button"
-            className="block p-2 text-3xl text-white md:hidden "
+            className="block p-2 text-3xl text-white md:hidden"
             aria-expanded={open}
             onClick={() => setOpen(true)}
           >
             <MdMenu />
-            <span className="sr-only">Open Menu</span>
+            <span className="sr-only">Open menu</span>
           </button>
         </div>
-
-        {/* Mobile NAV */}
+        {/* Mobile Nav */}
         <div
           className={clsx(
-            "fixed bottom-0 ring-0 left-0 top-0 z-40 flex flex-col items-end gap-4 bg-[#070815] pr-4 pt-14 transition-transform duration-300 ease-in-out motion-reduce:transform-none md:hidden",
+            "ga-4 fixed bottom-0 left-0 right-0 top-0 z-40 flex flex-col items-end bg-[#070815] pr-4 pt-14 transition-transform duration-300 ease-in-out motion-reduce:transition-none md:hidden",
             open ? "translate-x-0" : "translate-x-[100%]"
           )}
         >
           <button
             type="button"
-            className="block p-2 text-3xl text-white  fixed right-4 top-4 mb-4 md:hidden"
+            className="fixed right-4 top-4 mb-4 block p-2 text-3xl text-white md:hidden"
             aria-expanded={open}
             onClick={() => setOpen(false)}
           >
             <MdClose />
-            <span className="sr-only">Open Menu</span>
+            <span className="sr-only">Close menu</span>
           </button>
 
           <div className="grid justify-items-end gap-8">
@@ -91,20 +90,36 @@ export const NavBar = ({ settings }: NavBarProps) => {
           </div>
         </div>
 
-        <ul className="flex gap-6">
+        {/* Desktop Nav */}
+        <ul className=" hidden gap-6 md:flex">
           {settings.data.navigation.map((item) => {
             if (item.cta_button) {
               return (
                 <li key={item.label}>
-                  <ButtonLink field={item.link}>{item.label}</ButtonLink>
+                  <ButtonLink
+                    field={item.link}
+                    aria-current={
+                      pathname.includes(asLink(item.link) as string)
+                        ? "page"
+                        : undefined
+                    }
+                  >
+                    {item.label}
+                  </ButtonLink>
                 </li>
               );
             }
+
             return (
               <li key={item.label}>
                 <PrismicNextLink
-                  className="inline-flex max-h-11 items-center"
                   field={item.link}
+                  className="inline-flex min-h-11 items-center"
+                  aria-current={
+                    pathname.includes(asLink(item.link) as string)
+                      ? "page"
+                      : undefined
+                  }
                 >
                   {item.label}
                 </PrismicNextLink>
