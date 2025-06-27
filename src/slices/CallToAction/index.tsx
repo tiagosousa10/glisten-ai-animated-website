84b10cc2fad24a49959dfed7efce5a0bbd2e5b1f
tiagosousa <tiagosousa.tams@hotmail.com>
2025-06-27 17:34:34 +0100

    components added to the call to action slice

--- src/slices/CallToAction/index.tsx
+++ src/slices/CallToAction/index.tsx
@@ -1,8 +1,9 @@
 import { FC } from "react";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicText, SliceComponentProps } from "@prismicio/react";
 import PlainLogo from "./PlainLogo";
 import Bounded from "@/components/Bounded";
+import ButtonLink from "@/components/ButtonLink";
 
 /**
  * Props for `CallToAction`.
@@ -17,8 +18,20 @@ const CallToAction: FC<CallToActionProps> = ({ slice }) => {
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="relative py-32 text-center font-medium md:py-40"
     >
-      <PlainLogo />
+      <div className="glow absolute -z-10 aspect-square w-full max-w-xl rounded-full bg-blue-500/50 blur-[160px] filter" />
+      <div className="glass-container rounded-lg bg-gradient-to-b from-slate-800 to-slate-900 p-4 md:rounded-xl">
+        <PlainLogo />
+      </div>
+
+      <div className="mt-8 max-w-xl text-5xl  text-balance">
+        <PrismicText field={slice.primary.heading} />
+      </div>
+
+      <ButtonLink field={slice.primary.button_link} className="mt-6">
+        {slice.primary.button_text}
+      </ButtonLink>
     </Bounded>
   );
 };
