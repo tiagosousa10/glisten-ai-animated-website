f7c32f520d417bccbb46c40551181525d4ad8cf2
tiagosousa <tiagosousa.tams@hotmail.com>
2025-06-28 14:04:09 +0100

    integrations v2

--- src/slices/Integrations/AnimatedContent.tsx
+++ src/slices/Integrations/AnimatedContent.tsx
@@ -38,6 +38,11 @@ export default function AnimatedContent({
 
   useGSAP(
     () => {
+      if (prefersReducedMotion) {
+        gsap.set(container.current, { y: 0 });
+        return;
+      }
+
       const tl = gsap.timeline({
         repeat: -1,
         defaults: { ease: "power2.inOut" },
@@ -133,144 +138,3 @@ export default function AnimatedContent({
     </div>
   );
 }
-
-// "use client";
-
-// import StylizedLogoMark from "./StylizedLogoMark";
-// import clsx from "clsx";
-// import { Content } from "@prismicio/client";
-// import React, { useRef } from "react";
-// import gsap from "gsap";
-// import { useGSAP } from "@gsap/react";
-// import usePrefersReducedMotion from "@/hooks/usePrefersReducedMotion";
-// import {
-//   FaDigitalOcean,
-//   FaCloudflare,
-//   FaNpm,
-//   FaGithub,
-//   FaFigma,
-//   FaFly,
-// } from "react-icons/fa6";
-
-// export default function AnimatedContent({
-//   slice,
-// }: {
-//   slice: Content.IntegrationsSlice;
-// }) {
-//   const container = useRef(null);
-//   const prefersReducedMotion = usePrefersReducedMotion();
-//   gsap.registerPlugin(useGSAP); // register gsap to use in useGSAP hook
-
-//   const icons = {
-//     digitalocean: <FaDigitalOcean />,
-//     cloudflare: <FaCloudflare />,
-//     npm: <FaNpm />,
-//     github: <FaGithub />,
-//     figma: <FaFigma />,
-//     fly: <FaFly />,
-//   };
-
-//   useGSAP(
-//     () => {
-//       if (prefersReducedMotion) {
-//         gsap.set(container.current, { y: 0 });
-//         return;
-//       }
-//       const tl = gsap.timeline({
-//         repeat: -1,
-//         defaults: { ease: "power2.inOut" },
-//       });
-//       tl.to(".pulsing-logo", {
-//         kef: [
-//           {
-//             filter: "brightness(2)",
-//             opacity: 1,
-//             duration: 0.4,
-//             ease: "power2.in",
-//           },
-//           {
-//             filter: "brightness(1)",
-//             opacity: 0.7,
-//             duration: 0.9,
-//           },
-//         ],
-//       });
-
-//       tl.to(
-//         ".signal-line",
-//         {
-//           keyframes: [
-//             {
-//               backgroundPosition: "0% 0%",
-//             },
-//             {
-//               backgroundPosition: "100% 100%",
-//               stagger: { from: "center", each: 0.3 },
-//               duration: 1,
-//             },
-//           ],
-//         },
-//         "-=1.4"
-//       );
-
-//       tl.to(
-//         "pulsing-icon",
-//         {
-//           keyframes: [
-//             {
-//               opacity: 1,
-//               stagger: {
-//                 from: "center",
-//                 each: 0.3,
-//               },
-//               duration: 1,
-//             },
-//             {
-//               opacity: 0.4,
-//               stagger: {
-//                 from: "center",
-//                 each: 0.3,
-//               },
-//               duration: 1,
-//             },
-//           ],
-//         },
-//         "-=2"
-//       );
-//     },
-//     { scope: container }
-//   );
-
-//   return (
-//     <div
-//       className="mt-20 flex flex-col items-center md:flex-row"
-//       ref={container}
-//     >
-//       {slice.primary.repeatable.map((item, index) => {
-//         return (
-//           <React.Fragment key={index}>
-//             {index === Math.floor(slice.items.length / 2) && (
-//               <>
-//                 <StylizedLogoMark />
-//                 <div className="signal-line rotate-180 bg-gradient-to-t" />
-//               </>
-//             )}
-//             <div className="pulsing-icon flex aspect-square shrink-0 items-center justify-center rounded-full border border-blue-50/30 bg-blue-50/25 p-3 text-3xl text-blue-100 opacity-40 md:text-4xl lg:text-5xl">
-//               {item.icon && icons[item.icon]}
-//             </div>
-//             {index !== slice.primary.repeatable.length - 1 && (
-//               <div
-//                 className={clsx(
-//                   "signal-line",
-//                   index >= Math.floor(slice.primary.repeatable.length / 2)
-//                     ? "rotate-180"
-//                     : "rotate-0"
-//                 )}
-//               />
-//             )}
-//           </React.Fragment>
-//         );
-//       })}
-//     </div>
-//   );
-// }
