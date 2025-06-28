93f45eb71093a23b5971673fdffbac1d7e2f70fd
tiagosousa <tiagosousa.tams@hotmail.com>
2025-06-28 13:00:49 +0100

    animations with gsap

--- src/slices/Hero/AnimatedContent.tsx
+++ src/slices/Hero/AnimatedContent.tsx
@@ -4,37 +4,82 @@ import StarGrid from "@/components/StarGrid";
 import { Content, isFilled } from "@prismicio/client";
 import { PrismicNextImage } from "@prismicio/next";
 import { PrismicRichText, PrismicText } from "@prismicio/react";
-import React from "react";
+import React, { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 export default function AnimatedContent({
   slice,
 }: {
   slice: Content.HeroSlice;
 }) {
+  const container = useRef(null);
+  gsap.registerPlugin(useGSAP); // register gsap to use in useGSAP hook
+
+  useGSAP(
+    () => {
+      const tl = gsap.timeline({ defaults: { ease: "power2.inOut" } }); // control the animation timeline
+
+      tl.fromTo(
+        ".hero__heading",
+        { scale: 0.5 },
+        { scale: 1, opacity: 1, duration: 1.4 }
+      );
+      tl.fromTo(
+        ".hero__body",
+        { y: 20 },
+        { y: 0, opacity: 1, duration: 1.2 },
+        "-=0.6"
+      );
+      tl.fromTo(
+        ".hero__button",
+        { scale: 1.5 },
+        { scale: 1, opacity: 1, duration: 1.3 },
+        "-=0.8"
+      );
+      tl.fromTo(
+        ".hero__image",
+        { y: 100 },
+        { y: 0, opacity: 1, duration: 1.3 },
+        "+=0.3"
+      );
+      tl.fromTo(
+        ".hero__glow",
+        { scale: 0.5 },
+        { scale: 1, opacity: 1, duration: 1.8 },
+        "-=1"
+      );
+    },
+    { scope: container }
+  );
+
   return (
-    <div className="relative">
+    <div className="relative" ref={container}>
       <StarGrid />
       {isFilled.richText(slice.primary.heading) && (
-        <h1 className="text-balance text-5xl font-medium md:text-7xl ">
+        <h1 className="hero__heading text-balance text-5xl font-medium md:text-7xl opacity-0 ">
           <PrismicText field={slice.primary.heading} />
         </h1>
       )}
 
       {isFilled.richText(slice.primary.body) && (
-        <div className="mx-auto mt-6 max-w-md text-balance text-slate-300">
+        <div className="hero__body mx-auto mt-6 max-w-md text-balance text-slate-300 opacity-0">
           <PrismicRichText field={slice.primary.body} />
         </div>
       )}
 
       {isFilled.link(slice.primary.button_link) && (
-        <ButtonLink className="mt-8" field={slice.primary.button_link}>
+        <ButtonLink
+          className="mt-8 hero__button opacity-0"
+          field={slice.primary.button_link}
+        >
           {slice.primary.button_label}
         </ButtonLink>
       )}
 
       {isFilled.image(slice.primary.image) && (
-        <div className="glass-container mt-16 w-fit">
-          <div className="absolute inset-0 -z-10 bg-blue-500/30 blur-2xl filter" />
+        <div className="glass-container hero__image mt-16 w-fit opacity-0">
+          <div className="hero__glow absolute inset-0 -z-10 bg-blue-500/30 blur-2xl filter opacity-0" />
           <PrismicNextImage
             className="rounded-lg"
             field={slice.primary.image}
