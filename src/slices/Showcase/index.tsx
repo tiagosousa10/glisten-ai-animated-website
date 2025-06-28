ce13850dceb3bc57ad381ba229f9a3eecd9537d5
tiagosousa <tiagosousa.tams@hotmail.com>
2025-06-28 13:46:20 +0100

    showcase animations with gsap

--- src/slices/Showcase/index.tsx
+++ src/slices/Showcase/index.tsx
@@ -6,6 +6,7 @@ import { PrismicNextImage } from "@prismicio/next";
 import ButtonLink from "@/components/ButtonLink";
 import clsx from "clsx";
 import { PiArrowClockwise, PiGear } from "react-icons/pi";
+import AnimatedContent from "./AnimatedContent";
 
 const icon = {
   gear: <PiGear />,
@@ -28,16 +29,18 @@ const Showcase: FC<ShowcaseProps> = ({ slice }) => {
       className="relative"
     >
       <div className="glow absolute -z-10 aspect-square w-full max-w-xl rounded-full bg-blue-400/20 blur-3xl filter" />
-      <PrismicRichText
-        field={slice.primary.heading}
-        components={{
-          heading2: ({ children }) => (
-            <h2 className="text-5xl text-balance text-center font-medium md:text-7xl">
-              {children}
-            </h2>
-          ),
-        }}
-      />
+      <AnimatedContent>
+        <PrismicRichText
+          field={slice.primary.heading}
+          components={{
+            heading2: ({ children }) => (
+              <h2 className="text-5xl text-balance text-center font-medium md:text-7xl">
+                {children}
+              </h2>
+            ),
+          }}
+        />
+      </AnimatedContent>
       <div className="mt-16 grid items-center gap-8 rounded-xl border border-blue-50/20 bg-gradient-to-b from-slate-50/15 to-slate-50/5 px-8 py-8 backdrop-blur-sm lg:grid-cols-3 lg:gap-0 lg:py-12">
         <div>
           <div className="w-fit rounded-lg bg-blue-500/35 p-4 text-3xl">
