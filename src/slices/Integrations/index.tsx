ccca6deaacdae245f3b06748c5501a7343a46c4c
tiagosousa <tiagosousa.tams@hotmail.com>
2025-06-28 14:01:04 +0100

    integrations animations with gsap

--- src/slices/Integrations/index.tsx
+++ src/slices/Integrations/index.tsx
@@ -9,16 +9,7 @@ import Bounded from "@/components/Bounded";
 import StarBackground from "./StarBackground";
 import background from "./background.jpg";
 import Image from "next/image";
-import {
-  FaDigitalOcean,
-  FaCloudflare,
-  FaNpm,
-  FaGithub,
-  FaFigma,
-  FaFly,
-} from "react-icons/fa6";
-import StylizedLogoMark from "./StylizedLogoMark";
-import clsx from "clsx";
+import AnimatedContent from "./AnimatedContent";
 /**
  * Props for `Integrations`.
  */
@@ -28,15 +19,6 @@ export type IntegrationsProps = SliceComponentProps<Content.IntegrationsSlice>;
  * Component for "Integrations" Slices.
  */
 const Integrations: FC<IntegrationsProps> = ({ slice }) => {
-  const icons = {
-    digitalocean: <FaDigitalOcean />,
-    cloudflare: <FaCloudflare />,
-    npm: <FaNpm />,
-    github: <FaGithub />,
-    figma: <FaFigma />,
-    fly: <FaFly />,
-  };
-
   return (
     <Bounded
       data-slice-type={slice.slice_type}
@@ -62,33 +44,7 @@ const Integrations: FC<IntegrationsProps> = ({ slice }) => {
         </div>
 
         {/* TODO : FIX ICONS -> they arent mapping correctly */}
-        <div className="mt-20 flex flex-col items-center md:flex-row">
-          {slice.primary.repeatable.map(async (item, index) => {
-            return (
-              <React.Fragment key={index}>
-                {index === Math.floor(slice.items.length / 2) && (
-                  <>
-                    <StylizedLogoMark />
-                    <div className="signal-line rotate-180 bg-gradient-to-t" />
-                  </>
-                )}
-                <div className="pulsing-icon flex aspect-square shrink-0 items-center justify-center rounded-full border border-blue-50/30 bg-blue-50/25 p-3 text-3xl text-blue-100 opacity-40 md:text-4xl lg:text-5xl">
-                  {item.icon && icons[item.icon]}
-                </div>
-                {index !== slice.primary.repeatable.length - 1 && (
-                  <div
-                    className={clsx(
-                      "signal-line",
-                      index >= Math.floor(slice.primary.repeatable.length / 2)
-                        ? "rotate-180"
-                        : "rotate-0"
-                    )}
-                  />
-                )}
-              </React.Fragment>
-            );
-          })}
-        </div>
+        <AnimatedContent slice={slice} />
       </div>
     </Bounded>
   );
