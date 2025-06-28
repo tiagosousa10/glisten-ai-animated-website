e111f67b4f644fdf54daa8941bc0c14ab022fb28
tiagosousa <tiagosousa.tams@hotmail.com>
2025-06-28 12:45:46 +0100

    animated content added

---
+++ src/slices/Hero/AnimatedContent.tsx
@@ -0,0 +1,47 @@
+"use client";
+import ButtonLink from "@/components/ButtonLink";
+import StarGrid from "@/components/StarGrid";
+import { Content, isFilled } from "@prismicio/client";
+import { PrismicNextImage } from "@prismicio/next";
+import { PrismicRichText, PrismicText } from "@prismicio/react";
+import React from "react";
+
+export default function AnimatedContent({
+  slice,
+}: {
+  slice: Content.HeroSlice;
+}) {
+  return (
+    <div className="relative">
+      <StarGrid />
+      {isFilled.richText(slice.primary.heading) && (
+        <h1 className="text-balance text-5xl font-medium md:text-7xl ">
+          <PrismicText field={slice.primary.heading} />
+        </h1>
+      )}
+
+      {isFilled.richText(slice.primary.body) && (
+        <div className="mx-auto mt-6 max-w-md text-balance text-slate-300">
+          <PrismicRichText field={slice.primary.body} />
+        </div>
+      )}
+
+      {isFilled.link(slice.primary.button_link) && (
+        <ButtonLink className="mt-8" field={slice.primary.button_link}>
+          {slice.primary.button_label}
+        </ButtonLink>
+      )}
+
+      {isFilled.image(slice.primary.image) && (
+        <div className="glass-container mt-16 w-fit">
+          <div className="absolute inset-0 -z-10 bg-blue-500/30 blur-2xl filter" />
+          <PrismicNextImage
+            className="rounded-lg"
+            field={slice.primary.image}
+            alt=""
+          />
+        </div>
+      )}
+    </div>
+  );
+}
